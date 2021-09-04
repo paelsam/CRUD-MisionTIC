062a343672e9ca9328df8036b448199a9e59d866
Elkin Panameño <elkinsamir643@gmail.com>
2021-09-03 23:12:55 -0500

    CRUDv2

---
+++ read.py
@@ -0,0 +1,59 @@
+from datetime import datetime  # Aun no los necesitamos, asi que ignorenlo XD
+from openpyxl import load_workbook  # Modulo que nos traera el archivo Excel
+
+
+def leer(ruta: str, extraer: str):
+    # Importamos el archivo excel a travez de esta variable
+    archivo_excel = load_workbook(ruta)
+    # Seleccionamos la hoja que queremos sacar
+    hoja_datos = archivo_excel['Datos del CRUD']
+    # Extraemos la informacion desde la fila A2 hasta la maxima fila, F
+    hoja_datos = hoja_datos['A2':'F' + str(hoja_datos.max_row)]
+
+    info = {}  # Diccionario donde se guardaran los datos leidos desde el Excel
+
+    for i in hoja_datos:  # Recorremos la variable hoja_datos que es una tupla y sus llaves son las filas del documento Excel
+        if isinstance(i[0].value, int):
+            info.setdefault(i[0].value, {  # Guardamos los valores las filas en el diccionario
+                'tarea': i[1].value,
+                'descripcion': i[2].value,
+                'estado': i[3].value,
+                'fecha de inicio': i[4].value,
+                'fecha de finalizacion': i[5].value
+            })
+
+    def filtrar_estado(info, filtro):  # Para filtrar un archivo por estados.
+        for i in info:  # For que tomara los datos del Excel
+            if info[i]['estado'] == filtro:  # Condicional que filtrara el estado correspondiente
+                result = print(
+                        'ID:', str(i),
+                        '\nTitulo:', str(info[i]['tarea']),
+                        '\nDescripcion:', str(info[i]['descripcion']),
+                        '\nEstado:', str(info[i]['estado']),
+                        '\nFecha de inicio:', str(info[i]['fecha de inicio']),
+                        '\nFecha de finalizacion', str(
+                            info[i]['fecha de finalizacion'])
+                        )
+        return result
+
+    # Esto es por si queremos filtrar, mostrara todo si escribimos el estado que queramos filtrar.
+    if not(extraer == 'todo'):
+        print(filtrar_estado(info, extraer))
+    else:
+        for i in info:  # Si no, mostrara todo con esto :3
+            print('*********Tarea**********')
+            print(
+                'ID:', str(i),
+                '\nTitulo:', str(info[i]['tarea']),
+                '\nDescripcion:', str(info[i]['descripcion']),
+                '\nEstado:', str(info[i]['estado']),
+                '\nFecha de inicio:', str(info[i]['fecha de inicio']),
+                '\nFecha de finalizacion', str(info[i]['fecha de finalizacion']))
+            print('************************')
+    return
+
+
+# Si lo van a probar, tienen que cambiar la direccion de su archivo de Excel
+rut = r"C:\Users\kinmi\OneDrive\Escritorio\CRUD\docs\Base CRUD.xlsx"
+# Este solo filtrara, el estado. No quise poner un filtrador para los demas, porque haria mucho codigo :'v
+leer(rut, 'todo')
---
+++ update.py
@@ -0,0 +1,63 @@
+from openpyxl import load_workbook  # Modulo que nos traera el archivo Excel
+
+
+def actualizar(ruta: str, identificador: int, datos_actualizados: dict):
+    # Importamos el archivo excel a travez de esta variable
+    archivo_excel = load_workbook(ruta)
+    # Seleccionamos la hoja que queremos sacar
+    hoja_datos = archivo_excel['Datos del CRUD']
+    # Extraemos la informacion desde la fila A2 hasta la maxima fila, F
+    hoja_datos = hoja_datos['A2':'F' + str(hoja_datos.max_row)]
+    # La funcion 'active' nos ayuda a editar el documento Excel
+    hoja = archivo_excel.active
+
+    # Indicaremos las columnas de nuestro documento
+    titulo = 2
+    descripcion = 3
+    estado = 4
+    fecha_inicio = 5
+    fecha_finalizado = 6
+    encontro = False
+
+    for i in hoja_datos:
+        if i[0].value == identificador:
+            # La variable fila capturara la fila de su ID
+            fila = i[0].row
+            # La Flag Variable para mostrar que si encontro el ID
+            encontro = True
+            for d in datos_actualizados:
+                # Condicional para actualizar los valores
+                # Si el espacio esta vacio, no lo actualiza
+                if d == 'Titulo' and not(datos_actualizados[d] == ''):
+                    # La funcion 'cell' nos deja acceder a la celda.
+                    # Teniendo en cuenta la direccion de su fila y columna.
+                    hoja.cell(
+                        row=fila, colum=titulo).value = datos_actualizados[d]
+                elif d == 'Descripcion' and not(datos_actualizados[d] == ''):
+                    hoja.cell(
+                        row=fila, colum=descripcion).value = datos_actualizados[d]
+                elif d == 'Estado' and not(datos_actualizados[d] == ''):
+                    hoja.cell(
+                        row=fila, colum=estado).value = datos_actualizados[d]
+                elif d == 'Fecha Inicio' and not(datos_actualizados[d] == ''):
+                    hoja.cell(
+                        row=fila, colum=fecha_inicio).value = datos_actualizados[d]
+                elif d == 'Fecha Finalizado' and not(datos_actualizados[d] == ''):
+                    hoja.cell(
+                        row=fila, colum=fecha_finalizado).value = datos_actualizados[d]
+    # Guardamos el cambio con esta duncion
+    archivo_excel.save(ruta)
+    if encontro == False:
+        print("No se encontro el ID especificado. Intentalo de Nuevo")
+    return
+
+
+rut = r"C:\Users\kinmi\OneDrive\Escritorio\CRUD\docs\Base CRUD.xlsx"
+valores_actualizar = {
+    "Titulo": "loading...",
+    "Descripcion": "Nashee",
+    "Estado": "In process pa",
+    "Fecha Inicio": "Hoy",
+    "Fecha Finalizado": "Ayer"
+}
+actualizar()
